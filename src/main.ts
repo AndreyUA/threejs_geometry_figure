b8ce885de26cb246a55944e3232368b08997d4b3
AndreyUA <infernalsteel666@gmail.com>
2025-01-04 17:07:55 +0200

    initial setup for threejs

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,24 @@ import "./style.css";
 
 import * as THREE from "three";
 
-console.log("THREE >> ", THREE);
+const width = globalThis.innerWidth;
+const height = globalThis.innerHeight;
+
+const canvas = document.querySelector("canvas.webgl");
+
+if (!canvas) {
+  throw new Error("Canvas not found.");
+}
+
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas,
+});
+renderer.setSize(width, height);
+
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10);
+camera.position.z = 2;
+
+const scene = new THREE.Scene();
+
+renderer.render(scene, camera);
