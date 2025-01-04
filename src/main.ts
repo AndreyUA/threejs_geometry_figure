e7325b339f6efdcf0e0a656133fc1e68679bd6dc
AndreyUA <infernalsteel666@gmail.com>
2025-01-04 17:43:42 +0200

    added orbit control

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import "./style.css";
 
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const width = globalThis.innerWidth;
 const height = globalThis.innerHeight;
 
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
 if (!canvas) {
   throw new Error("Canvas not found.");
@@ -38,12 +39,20 @@ const wireMesh = new THREE.Mesh(geometry, wireMaterial);
 wireMesh.scale.setScalar(1.001);
 mesh.add(wireMesh);
 
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+
 const hemisphereLight = new THREE.HemisphereLight(0x0099ff, 0xaa5500, 1);
 scene.add(hemisphereLight);
 
 const tick: FrameRequestCallback = (time: number) => {
   globalThis.requestAnimationFrame(tick);
+
   mesh.rotation.y = time * 0.0001;
+
+  controls.update();
+
   renderer.render(scene, camera);
 };
 
